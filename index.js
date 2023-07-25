9efb7505d98d766cec0428eb38c368c80ad6ecd6
Ross F <rfahan@gmail.com>
2023-07-24 21:56:06 -0600

    added questions for user input

--- index.js
+++ index.js
@@ -5,7 +5,52 @@ const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    
+  {
+    type: "input",
+    message: "Enter your project title:",
+    name: "projTitle"
+  },
+  {
+    type: "input",
+    message: "Enter a description of your project:",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "Enter installation instructions:",
+    name: "installInst"
+  },
+  {
+    type: "input",
+    message: "Enter usage information:",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "Enter contribution guidelines:",
+    name: "contributions"
+  },
+  {
+    type: "input",
+    message: "Enter test instructions:",
+    name: "testing"
+  },
+  {
+    type: "list",
+    message: "Choose a license for your application:",
+    name: "license",
+    choices: ["MIT", "Apache", "GPL", "BSD", "Mozilla", "None"]
+  },
+  {
+    type: "input",
+    message: "Enter your GitHub username:",
+    name: "userName"
+  },
+  {
+    type: "input",
+    message: "Enter your email address:",
+    name: "email"
+  }
 ];
 
 // TODO: Create a function to write README file
@@ -13,7 +58,7 @@ function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() { 
-    
+
     }
 
 // Function call to initialize app
