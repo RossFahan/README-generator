fe1b44649a099ec439e1977eeb18bffe2880953b
Ross F <rfahan@gmail.com>
2023-07-25 21:48:42 -0600

    completed renderLicenseSection function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -21,28 +21,44 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
-switch (license) {
-  case 'MIT':
-    return '(https://opensource.org/license/mit/)';
-  case 'Apache':
-    return '(https://opensource.org/license/apache-2-0/)';
-  case 'GPL':
-    return '(https://opensource.org/license/gpl-2-0/)';
-  case 'BSD':
-    return '(https://opensource.org/license/bsd-2-clause/)';
-  case 'Mozilla':
-    return '(https://opensource.org/license/mpl-2-0/)';
-  case 'none':
-    return '';
-  default:
-    return '';
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return '(https://opensource.org/license/mit/)';
+    case 'Apache':
+      return '(https://opensource.org/license/apache-2-0/)';
+    case 'GPL':
+      return '(https://opensource.org/license/gpl-2-0/)';
+    case 'BSD':
+      return '(https://opensource.org/license/bsd-2-clause/)';
+    case 'Mozilla':
+      return '(https://opensource.org/license/mpl-2-0/)';
+    case 'none':
+      return '';
+    default:
+      return '';
+  }
 }
-}
-
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if ((license) && (license !== 'none')) {
+
+    const licenseLink = renderLicenseLink(license);
+    const licenseBadge = renderLicenseBadge(license);
+
+    return `## License
+
+| License | Link |
+| ------- | ---- |
+| ${licenseBadge} | [${licenseLink}](${licenseLink}) |
+
+`;
+  }
+  else {
+    return ''; //No license
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
